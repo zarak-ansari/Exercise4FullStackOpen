6c1c6e6fc2d45d4eb45430853765d3f9a1a42c4b
Zarak Ansari <zarak.ansari@yahoo.com>
2023-05-24 15:52:53 +0500

    completed 4-2

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-const blogRouter = require('./controllers/blogs')
+const app = require('./app')
 const config = require('./utils/config')
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
