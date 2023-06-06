b92f178c808e00ed346fefb654250dfed29c4ae6
Zarak Ansari <zarak.ansari@yahoo.com>
2023-06-06 17:21:45 +0500

    4-16 WIP

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors')
 
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,23 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
-const User = require('../models/user')
+const User = require('../models/users')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if(!username || !password) {
+    response.status(400).send({error:"missing username or password"})
+  }
+
+  if((username.length < 3) || (password.length < 3)) {
+    response.status(400).send({error:"username and password must be atleast 3 characters long"})
+  }
+
+  const existingUser = User.find({username:username})
+
+  if(existingUser){
+    response.status(400).send({error:"user with this username already exists"})    
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
